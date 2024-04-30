5ef9d0327c49ed250d83b725610e0532fb7bba5f
vikas-saini-7 <vikas289888@gmail.com>
2024-04-30 12:17:00 +0530

    header scroll change

--- client/src/components/common/CarouselSlide.jsx
+++ client/src/components/common/CarouselSlide.jsx
@@ -4,22 +4,32 @@ import { IconPlayerPlay } from '@tabler/icons-react';
 
 const baseUrl = 'https://image.tmdb.org/t/p/original';
 
+const truncateDescription = (description, maxLength) => {
+  if (description.length <= maxLength) {
+    return description;
+  }
+  return description.substr(0, maxLength) + "...";
+}
+
 const CarouselSlide = ({slide}) => {
-  const {backdrop_path, title, overview, rating, tags} = slide;
+  const {backdrop_path, title, overview, vote_average, tags} = slide;
+
+  const truncatedDescription = truncateDescription(overview, 100);
+
   return (
     <div className='relative bg-cover pt-14' style={{backgroundImage:`url(${baseUrl}${backdrop_path})`}}>
       <div style={{backgroundImage: "linear-gradient(to top, #0C0C0C, rgba(0,0,0,0))"}} className='absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50'></div>
-      <div className='relative pt-28 lg:pt-44 px-[10%]'>
+      <div className='relative pt-28 lg:pt-44 px-[10%] min-h-[450px] lg:min-h-[700px]'>
         <div className='flex flex-col gap-8 max-w-[480px]'>
           <h1 className='text-5xl lg:text-7xl font-bold'>{title}</h1>
-          <div className='flex gap-2 items-center'>
-            <p className='w-12 h-12 flex items-center justify-center mr-2 text-lg font-bold'>{rating}</p>
+          <div className='flex items-center'>
+            <p className='w-12 h-12 flex items-center justify-center mr-2 text-lg font-bold'>{vote_average}</p>
             {/* <CircularProgress value={4} /> */}
             {/* {tags.map((item) => (
               <p className='bg-primary rounded-full px-5 py-2 text-sm'>{item}</p>
             ))} */}
           </div>
-          <p className='text-lg'>{overview} Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet! </p>
+          <p className='text-lg'>{truncatedDescription}</p>
           <button className='button button-large w-fit flex items-center gap-2'> <IconPlayerPlay/> Watch Now</button>
         </div>
       </div>
--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IconMenu, IconMoon } from '@tabler/icons-react'
 import { Link, NavLink } from 'react-router-dom'
 import MyModal from './MyModal'
 
 const Header = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const isScrolled = window.scrollY > 100; // Change 100 to your desired scroll position
+        setScrolled(isScrolled);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
   return (
-    <div className='header'>
+    <div className={`header transition-all duration-500 ${scrolled ? 'backdrop-blur-md' : ''}`}>
         <div className='flex items-center gap-6'>
             <h1 className='font-black text-primary text-3xl'>DarkFlix</h1>
             <ul className='flex items-center gap-4 text-md font-semibold uppercase'>
@@ -28,12 +43,6 @@ const Header = () => {
         </div>
         <nav>
         <ul>
-            {/* <li className='cursor-pointer'>
-                <IconMenu/>
-            </li> */}
-            {/* <div className='cursor-pointer button'>
-                Sign In
-            </div> */}
             <MyModal/>
         </ul>
         </nav>
