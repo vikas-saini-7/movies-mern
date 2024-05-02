b9ed9f92d220d19eb0fa9db231b11ee2da063063
vikas-saini-7 <vikas289888@gmail.com>
2024-05-02 18:19:00 +0530

    search page created

--- client/src/pages/search/SearchPage.jsx
+++ client/src/pages/search/SearchPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const SearchPage = () => {
-  return (
-    <div className='container mx-auto px-4 pt-14'>SearchPage</div>
-  )
-}
+  const [activeItem, setActiveItem] = useState('Movie');
 
-export default SearchPage
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <div className='container mx-auto px-4 pt-14'>
+      <div className='flex items-center justify-center gap-4 uppercase mt-8'>
+        <p
+          className={`cursor-pointer px-4 py-2 rounded ${activeItem === 'Movie' ? 'bg-primary bg-opacity-20' : ''}`}
+          onClick={() => handleItemClick('Movie')}
+        >
+          Movie
+        </p>
+        <p
+          className={`cursor-pointer px-4 py-2 rounded ${activeItem === 'Tv' ? 'bg-primary bg-opacity-20' : ''}`}
+          onClick={() => handleItemClick('Tv')}
+        >
+          Tv
+        </p>
+        <p
+          className={`cursor-pointer px-4 py-2 rounded ${activeItem === 'People' ? 'bg-primary bg-opacity-20' : ''}`}
+          onClick={() => handleItemClick('People')}
+        >
+          People
+        </p>
+      </div>
+      <div className='max-w-[800px] mx-auto'>
+        <input className='mt-8 border bg-transparent border-gray-500 border-opacity-20 w-full h-[50px] rounded-lg px-4 text-lg' type="text" placeholder='Search DarkFlix' />
+      </div>
+      <div className='min-h-[500px]'></div>
+    </div>
+  );
+};
+
+export default SearchPage;
