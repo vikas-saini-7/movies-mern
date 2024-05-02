516bfc6c8851fc8aa3066ccb7c1631cd48d6f951
vikas-saini-7 <vikas289888@gmail.com>
2024-05-03 01:34:49 +0530

    sm

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGODB_URI; // Get MongoDB URI from environment variables
+    const dbURI = process.env.MONGO_URI; // Get MongoDB URI from environment variables
     await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (error) {
