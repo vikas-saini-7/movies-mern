c7fce616d8b067fbe6bd5d91ada3052899877313
vikas-saini-7 <vikas289888@gmail.com>
2024-07-15 14:47:25 +0530

    resuming backend

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
               <PhoneMenu/>
             </Drawer>
             <h1 className='font-black text-primary text-3xl'>DarkFlix</h1>
-            <ul className='items-center gap-4 text-md font-semibold uppercase hidden md:flex'>
+            <ul className='items-center gap-4 ml-2 text-md font-semibold uppercase hidden md:flex'>
                 {/* <NavLink to='/'>
                     <li className='active'>Home</li>
                 </NavLink> */}
