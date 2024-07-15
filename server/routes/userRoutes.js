25de41f6ed09506a1b1c5dde7b05ff6cd4aebdc2
vikas-saini-7 <vikas289888@gmail.com>
2024-07-15 19:36:59 +0530

    created comment system api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/authMiddleware');
+const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post('/signup', userController.signup); //working
-router.post('/login', userController.login); //working
+router.post("/signup", userController.signup); //working
+router.post("/login", userController.login); //working
 
-router.post('/validateToken',authMiddleware , userController.validateToken); //working
+router.post("/validateToken", authMiddleware, userController.validateToken); //working
 
 module.exports = router;
